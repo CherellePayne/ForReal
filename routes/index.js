3a1a14488eb09130e2c1fd1035beed7166909cfd
CherellePayne <cherellelpayne@gmail.com>
2023-05-27 17:57:43 -0600

    error in

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-//added line 6 for Lo6 for validation 
-router.use('/user,require'('./user'));
 router.use('/temples', require('./temples'));
 
 module.exports = router;
