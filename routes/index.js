f7d06b8f4fb321736d66aac86923293c08d47e00
CherellePayne <cherellelpayne@gmail.com>
2023-06-10 21:06:04 -0600

    changes to .env and server

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,38 @@
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER
+};
 const express = require('express');
 const router = express.Router();
+
+router.use(auth(config));
+router.get('/checkingLoginStatus', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+ });
 
 router.use('/', require('./swagger'));
 router.use('/temples', require('./temples'));
 
+module.exports = router;
+
 // from OAuth video
-// router.get('/', (req, res) => {
+// router.get("/", (req, res) => {
 //     console.log (req.oidc.isAuthenticated());
-// res.render("index", {title: "Express Demo"});
+//     res.render("index", {
+//         title: "Express Demo"
+//         isAuthenticated: req.oidc.isAuthenticated(),
+// });
 // });
 
 // app.get('/', (req, res) => {
 //     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 //   });
 
-module.exports = router;
--- server.js
+++ server.js
@@ -4,19 +4,7 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 // const port = process.env.PORT || 3001;//cahnged for week 8
 const port = process.env.PORT;
-const { auth } = require('express-openid-connect');
-require('dotenv').config()
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASEURL,
-  clientID: process.env.CLIENTID,
-  issuerBaseURL: process.env.ISSUER,
-};
-
-app.use(auth(config));
 
 app
   .use(bodyParser.json())
@@ -35,27 +23,9 @@ mongodb.initDb((err) => {
   }
 });
 
+app.get('/', (req, res) => {
+      res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    });
 
-  //Used in team assignment video- minimal way
-  // process.on('uncaughtException', (err, origin) => {
-  //   console.log(process.stderr.id, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-  // });
 
-  // catch err
-  // const db = require('./models');
-  // db.mongoose
-  //   .connect(db.url, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true
-  //   })
-  //   .then(() => {
-  //     app.listen(port, () => {
-  //       console.log(`DB Connected and server running on ${port}.`);
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log('Cannot connect to the database!', err);
-  //     process.exit();
-  //   });
- 
   
